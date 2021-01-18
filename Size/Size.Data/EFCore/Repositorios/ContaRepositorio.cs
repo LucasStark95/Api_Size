7b1d3dcb60ac16b3a0fddda89be9259dbfdb3436
Antonio Lucas <lucas.d1995@gmail.com>
2021-01-18 19:59:22 -0300

    [Backend] - Alteração nos métodos do repositórios.

--- Size/Size.Data/EFCore/Repositorios/ContaRepositorio.cs
+++ Size/Size.Data/EFCore/Repositorios/ContaRepositorio.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Size.Core.Models;
+using System;
 using System.Threading.Tasks;
 
 namespace Size.Data.EFCore.Repositorios
@@ -12,22 +13,72 @@ namespace Size.Data.EFCore.Repositorios
 
         public async Task<Conta> DepositaAsync(int idCliente, double valor)
         {
-            return null;
+
+            ValidateValue(valor);
+
+            var contaDb = await GetContaAsync(idCliente);
+
+            ValidateConta(contaDb);
+
+            contaDb.Saldo += valor;
+
+            await AtualizarAsync(contaDb);
+
+            return contaDb;
         }
 
         public async Task<Conta> SaqueAsync(int idCliente, double valor)
         {
-            return null;
+            ValidateValue(valor);
+
+            var contaDb = await GetContaAsync(idCliente);
+
+            ValidateConta(contaDb);
+
+            if (contaDb.Saldo - valor < 0) throw new Exception("Saldo Insuficiente para essa Operação.");
+
+            contaDb.Saldo -= valor;
+
+            await AtualizarAsync(contaDb);
+
+            return contaDb;
         }
 
         public async Task<Conta> ExtratoAsync(int idCliente)
         {
-            return null;
+            return await Buscar(c => c.ClienteId == idCliente).FirstOrDefaultAsync();
         }
 
         public async Task<bool> TransferirAsync(int idCliente, int idClienteDestino, double valor)
         {
-            return false;
+            try
+            {
+                ValidateValue(valor);
+
+                await SaqueAsync(idCliente, valor);
+                await DepositaAsync(idClienteDestino, valor);
+
+                return true;
+            }
+            catch
+            {
+                throw new Exception("Problema ao realizar essa operação."); ;
+            }
+        }
+
+        private async Task<Conta> GetContaAsync(int idCliente)
+        {
+            return await Buscar(c => c.ClienteId == idCliente).FirstOrDefaultAsync();
+        }
+
+        private void ValidateValue(double valor)
+        {
+            if (valor < 0) throw new Exception("Valor não permitido.");
+        }
+
+        private void ValidateConta(Conta conta)
+        {
+            if (conta == null) throw new NullReferenceException("Conta não Localizada.");
         }
     }
 }
